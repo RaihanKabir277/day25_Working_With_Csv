72d9762772cb517d71d25268c2c6f854de6eddad
RaihanKabir277 <raihank0198@gmail.com>
2025-07-17 18:02:07 +0600

    pandas basic added here as we have to work in more complex data

--- main.py
+++ main.py
@@ -8,22 +8,35 @@
 # -------------there will be \n and also comma and others thing so we need to cleen this ----
 # ------ below the way , where do not need to clean the data ----------
 
-import csv
+# import csv
 
-with open("weather_data.csv", "r") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    # next(data)
-    # for row in data:
-    #     # print(row)
-    #     temperatures.append(int(row[1]))
-    # ----------another way --------
-    # for row in data[1:]:
-    #     temperatures.append(int(row[1]))
-    # ---------its work if we convert the data into list like data = list(csv.reader(data_file))
+# with open("weather_data.csv", "r") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     # next(data)
+#     # for row in data:
+#     #     # print(row)
+#     #     temperatures.append(int(row[1]))
+#     # ----------another way --------
+#     # for row in data[1:]:
+#     #     temperatures.append(int(row[1]))
+#     # ---------its work if we convert the data into list like data = list(csv.reader(data_file))
 
-    # -----------anoyher way to do this --------
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
+#     # -----------anoyher way to do this --------
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+# print(temperatures)
+
+
+# ---------------but when the files has more complex rows and columns we need pandas-----------
+
+import pandas
+data = pandas.read_csv("weather_data.csv")
+# print(data)
+
+# --------for only temperatures -----
+
+temperatures = data["temp"]
 print(temperatures)
+
