a3bbf244909f74087981b76193c32f95dcbc9562
RaihanKabir277 <raihank0198@gmail.com>
2025-07-17 17:51:46 +0600

    how csv file retrived more cleanly added here

---
+++ main.py
@@ -0,0 +1,29 @@
+# --------open with traditional method ----------
+
+# with open("weather_data.csv", "r") as file:
+#     data = file.readlines()
+#     print(data)
+
+# ---------------but its a problem in traditional method 
+# -------------there will be \n and also comma and others thing so we need to cleen this ----
+# ------ below the way , where do not need to clean the data ----------
+
+import csv
+
+with open("weather_data.csv", "r") as data_file:
+    data = csv.reader(data_file)
+    temperatures = []
+    # next(data)
+    # for row in data:
+    #     # print(row)
+    #     temperatures.append(int(row[1]))
+    # ----------another way --------
+    # for row in data[1:]:
+    #     temperatures.append(int(row[1]))
+    # ---------its work if we convert the data into list like data = list(csv.reader(data_file))
+
+    # -----------anoyher way to do this --------
+    for row in data:
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+print(temperatures)
